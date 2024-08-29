a476f83b141f08f837f010c501f715e3f7f2fdbb
Ayush <ayushdesai1000@gmail.com>
2024-08-28 17:07:44 -0700

    Initial Commit

---
+++ CoinMate/CoinMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinMateApp.swift
+//  CoinMate
+//
+//  Created by Ayush Desai on 8/28/24.
+//
+
+import SwiftUI
+
+@main
+struct CoinMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
