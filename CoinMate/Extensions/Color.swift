e1ae768ca6765522acda56bb956c414bc2bbe0df
Ayush <ayushdesai1000@gmail.com>
2024-08-30 22:38:56 -0700

    Base Commit

---
+++ CoinMate/Extensions/Color.swift
@@ -0,0 +1,21 @@
+//
+//  Color.swift
+//  CoinMate
+//
+//  Created by Ayush Desai on 8/30/24.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color{
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme{
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+}
